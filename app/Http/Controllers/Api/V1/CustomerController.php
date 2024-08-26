7f00907b54e427afadf3289ac32d40ae84030f4a
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-26 17:07:57 +0400

    Filter data

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,13 +8,21 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index() {
-        return new CustomerCollection(Customer::paginate());
+    public function index(Request $request) {
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); // [['column', 'operator', 'value']]
+        if(count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+        } else {
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
     }
 
     /**
