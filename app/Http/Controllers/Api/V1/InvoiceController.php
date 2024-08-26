05684de970544db0c21ece014b59e34dda5f02af
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-26 16:08:58 +0400

    Version & define routes

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -1,66 +1,59 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
-use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Requests\UpdateInvoiceRequest;
+use App\Models\Invoice;
 
-class CustomerController extends Controller
-{
+class InvoiceController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
         //
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCustomerRequest $request)
-    {
+    public function store(StoreInvoiceRequest $request) {
         //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Customer $customer)
-    {
+    public function show(Invoice $invoice) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customer $customer)
-    {
+    public function edit(Invoice $invoice) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
-    {
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice) {
         //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customer $customer)
-    {
+    public function destroy(Invoice $invoice) {
         //
     }
 }
