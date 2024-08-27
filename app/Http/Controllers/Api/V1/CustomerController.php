041f6fe1b573adaa2694a3588159408d361690d0
JalilAbdullayev <abdullacelil00@gmail.com>
2024-08-27 15:26:20 +0400

    Update

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\CustomerFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Requests\V1\StoreCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
@@ -48,7 +48,7 @@ class CustomerController extends Controller {
      * Update the specified resource in storage.
      */
     public function update(UpdateCustomerRequest $request, Customer $customer) {
-        //
+        $customer->update($request->all());
     }
 
     /**
---
+++ app/Http/Requests/V1/UpdateCustomerRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests\V1;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+
+class UpdateCustomerRequest extends FormRequest {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array {
+        $method = $this->method();
+        if($method === 'PUT') {
+            return [
+                'name' => ['required', 'string', 'max:255'],
+                'type' => ['required', 'string', 'max:255', Rule::in('I', 'B', 'i', 'b')],
+                'email' => ['required', 'string', 'email', 'max:255'],
+                'address' => ['required', 'string', 'max:255'],
+                'city' => ['required', 'string', 'max:255'],
+                'state' => ['required', 'string', 'max:255'],
+                'postal_code' => ['required', 'string', 'max:255'],
+            ];
+        }
+
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'type' => ['sometimes', 'required', 'string', 'max:255', Rule::in('I', 'B', 'i', 'b')],
+            'email' => ['sometimes', 'required', 'string', 'email', 'max:255'],
+            'address' => ['sometimes', 'required', 'string', 'max:255'],
+            'city' => ['sometimes', 'required', 'string', 'max:255'],
+            'state' => ['sometimes', 'required', 'string', 'max:255'],
+            'postal_code' => ['sometimes', 'required', 'string', 'max:255'],
+        ];
+    }
+
+    public function prepareForValidation(): void {
+        if($this->postalCode) {
+            $this->merge([
+                'type' => Str::upper($this->type)
+            ]);
+        }
+        if($this->type) {
+            $this->merge([
+                'postal_code' => $this->postalCode
+            ]);
+        }
+    }
+}
